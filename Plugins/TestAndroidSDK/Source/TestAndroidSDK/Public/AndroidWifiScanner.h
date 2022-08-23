3fc749d71b49bb598f528b8bd445218cf061881b
Kazuki Hamada <kzkhmdc@gmail.com>
2022-08-23 21:12:03 +0900

    Add AndroidWifiScanner

---
+++ Plugins/TestAndroidSDK/Source/TestAndroidSDK/Public/AndroidWifiScanner.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AndroidWifiScanner.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTANDROIDSDK_API UAndroidWifiScanner : public UObject
+{
+	GENERATED_BODY()
+	
+};
